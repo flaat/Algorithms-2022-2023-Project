0a83d44144b688a9fe67c34beaf0042e49e00dda
flaviogiorgi <giorgi.flavio.at@gmail.com>
2023-01-05 19:50:23 +0100

    Minor changes

---
+++ src/utility/plot_utls.py
@@ -0,0 +1,11 @@
+# This file contains all the constant you can use to make wounderful plots
+
+COLORS = ["r", "g", "b", "m", "c"]
+LINE_STYLES = []
+
+FONT_SIZE = 14
+LEGEND_SIZE = 14
+
+X_LABEL = "Days"
+Y_LABEL = { "price" : "Price (US Dollar)", 
+            "volume": "Volume (US Dollar)"}
